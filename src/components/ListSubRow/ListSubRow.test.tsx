c71a1edaf7b5e623a51fcda718dae5321ada80ec
Fergus Farrell <fergusf@orionhealth.com>
2019-09-20 18:06:16 +1200

    fixed failing tests

--- src/components/ListSubRow/ListSubRow.test.tsx
+++ src/components/ListSubRow/ListSubRow.test.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ListSubRow from "./ListSubRow";
 
+import { subData } from "../../constants";
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<ListSubRow />, div);
+  ReactDOM.render(<ListSubRow values={subData} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
