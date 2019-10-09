77af405577e96c5d73b0ced4574e1ccb77fc746d
Fergus Farrell <fergusf@orionhealth.com>
2019-10-09 15:53:31 +1300

    fixed up list row tests

--- src/components/ListSubRow/ListSubRow.test.tsx
+++ src/components/ListSubRow/ListSubRow.test.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "../../store";
 import ListSubRow from "./ListSubRow";
 
 it("renders list sub row without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <ListSubRow
-      rivers={[
-        { river_name: "test name", section_name: "test section" },
-        { river_name: "test name", section_name: "test section 2" }
-      ]}
-    />,
+    <Provider store={store}>
+      <ListSubRow
+        rivers={[
+          { river_name: "test name", section_name: "test section" },
+          { river_name: "test name", section_name: "test section 2" }
+        ]}
+      />
+    </Provider>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
