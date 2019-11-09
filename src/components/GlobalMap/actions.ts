7cc6cbdd08fd82201fd6b287c435e7ed9cefee3c
Fergus Farrell <fergusf@orionhealth.com>
2019-11-09 14:19:28 +1300

    persisted map layer and pitch to state

--- src/components/GlobalMap/actions.ts
+++ src/components/GlobalMap/actions.ts
@@ -1,5 +1,5 @@
 import { ThunkDispatch } from "redux-thunk";
-import { SET_CENTER, SET_ZOOM } from "./actionTypes";
+import { SET_CENTER, SET_ZOOM, SET_LAYER, SET_PITCH } from "./actionTypes";
 
 export const setCenter = (center: number[]) => (
   dispatch: ThunkDispatch<{}, {}, any>
@@ -16,5 +16,23 @@ export const setZoom = (zoom: number) => (
   dispatch({
     type: SET_ZOOM,
     payload: zoom
+  });
+};
+
+export const setLayer = (isSatellite: boolean) => (
+  dispatch: ThunkDispatch<{}, {}, any>
+) => {
+  dispatch({
+    type: SET_LAYER,
+    payload: isSatellite
+  });
+};
+
+export const setPitch = (pitch: number) => (
+  dispatch: ThunkDispatch<{}, {}, any>
+) => {
+  dispatch({
+    type: SET_PITCH,
+    payload: pitch
   });
 };
--- src/components/GlobalMap/reducer.ts
+++ src/components/GlobalMap/reducer.ts
@@ -1,8 +1,10 @@
-import { SET_CENTER, SET_ZOOM } from "./actionTypes";
+import { SET_CENTER, SET_ZOOM, SET_LAYER, SET_PITCH } from "./actionTypes";
 
 const initialState = {
   zoom: 5,
-  center: [172.186, -40.5]
+  center: [172.186, -40.5],
+  layer: false,
+  pitch: 0
 };
 
 const setCenter = (state: any, action: any) => ({
@@ -15,9 +17,21 @@ const setZoom = (state: any, action: any) => ({
   zoom: action.payload
 });
 
+const setLayer = (state: any, action: any) => ({
+  ...state,
+  layer: action.payload
+});
+
+const setPitch = (state: any, action: any) => ({
+  ...state,
+  pitch: action.payload
+});
+
 const actionMap: { [key: string]: any } = {
   [SET_CENTER]: setCenter,
-  [SET_ZOOM]: setZoom
+  [SET_ZOOM]: setZoom,
+  [SET_LAYER]: setLayer,
+  [SET_PITCH]: setPitch
 };
 
 export default function(state = initialState, action: any) {
